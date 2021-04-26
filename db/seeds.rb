49b21f31b26de2fda7c007b4a429bf79a5c07b7f
Jake Flynn <JakeFlynn@Jakes-MBP-4.attlocal.net>
2021-04-26 16:56:58 -0400

    test seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
 Vehicle.create([
-    { make: "Ferrari", model: "California", year: 2020, description, price, img_url, availability,}
+    { make: "Ferrari", model: "California", year: 2020, description: Faker::Vehicle.standard_specs, price: 500, img_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/94/Ferrari_California_-_Flickr_-_Alexandre_Pr%C3%A9vot_%2811%29.jpg/1200px-Ferrari_California_-_Flickr_-_Alexandre_Pr%C3%A9vot_%2811%29.jpg", availability: false },
+
+    { make: "Ferrari", model: "California", year: 2020, description: Faker::Vehicle.standard_specs, price: 500, img_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/94/Ferrari_California_-_Flickr_-_Alexandre_Pr%C3%A9vot_%2811%29.jpg/1200px-Ferrari_California_-_Flickr_-_Alexandre_Pr%C3%A9vot_%2811%29.jpg", availability: true },
+
+    { make: "Ferrari", model: "California", year: 2020, description: Faker::Vehicle.standard_specs, price: 500, img_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/94/Ferrari_California_-_Flickr_-_Alexandre_Pr%C3%A9vot_%2811%29.jpg/1200px-Ferrari_California_-_Flickr_-_Alexandre_Pr%C3%A9vot_%2811%29.jpg", availability: true }
 ])
